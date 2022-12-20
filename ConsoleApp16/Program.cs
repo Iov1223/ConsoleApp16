40ce632696f1e5a914920a4b9e3fbf06772e13f0
Andrey Popov <andrej_1987@bk.ru>
2022-12-20 18:41:57 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        public int GreatestCommonDivisor(int a, int b)
+        {
+            if (a % b == 0)
+                return b;
+            if (b % a == 0)
+                return a;
+            if (a > b)
+                return GreatestCommonDivisor(a % b, b);
+            return GreatestCommonDivisor(a, b % a);
+        }
+        public int LeastCommonMultiple(int a, int b)
+        {
+            return (a * b) / GreatestCommonDivisor(a, b);
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Program program = new Program();
+            int minSmallestCommonMultiple = program.LeastCommonMultiple(n - 1, 1), SmallestCommonMultiple;
+            int a = n - 1, b = 1;
+            for (int i = 2; i < n; i++)
+            {
+                SmallestCommonMultiple = program.LeastCommonMultiple(n - i, i);
+                if (minSmallestCommonMultiple > SmallestCommonMultiple)
+                {
+                    minSmallestCommonMultiple = SmallestCommonMultiple;
+                    a = n - i;
+                    b = i;
+                }
+            }
+            Console.WriteLine(a + " " + b); 
+        }
+    }
+}
